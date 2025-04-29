1cd1836f03439d7d4f895a23112db9d0e3c9b718
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-04-29 15:11:51 +0100

    refactor: comment out unused ProverbPage import and component

--- pages/clients/Proverb.js
+++ pages/clients/Proverb.js
@@ -1,10 +1,9 @@
 import Layout from '../../components/Layout/Layout';
-import ProverbPage from '../../components/Layout/ProverbPage';
+//import ProverbPage from '../../components/Layout/ProverbPage';
 
 function Proverb() {
   return (
     <Layout>
-      < ProverbPage/>
     </Layout>
   );
 }
